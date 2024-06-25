0132c07c085da44649354c5c017035e38aecc6d4
sukjinwebsit <sukritjindal@gmail.com>
2024-06-25 14:45:58 +0530

    Update smoothgrad.py

--- smoothgrad.py
+++ smoothgrad.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
 
-device = torch.device("cpu")
+device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+device = torch.device("cuda") if torch.cuda.is_available() else device
 
 class SmoothGrad():
     def __init__ (self, model, load, preprocess, saliency):
