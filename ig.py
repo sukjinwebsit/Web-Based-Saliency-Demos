33443122a28ce79c07d0eca33ac6e949e54fbe94
sukjinwebsit <sukritjindal@gmail.com>
2024-06-25 13:49:56 +0530

    Update ig.py

--- ig.py
+++ ig.py
@@ -3,8 +3,7 @@ import torch
 import numpy as np
 from torchvision import transforms
 
-device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
-device = torch.device("cuda") if torch.cuda.is_available() else device
+device = torch.device("cpu")
 
 class IG():
     def __init__(self, model, load, preprocess):
